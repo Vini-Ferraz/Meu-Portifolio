50cba114b5fce773e907b03023440a89b2521cce
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-04-19 15:49:44 -0300

    Adicionando titulo funcional
    Adicionei um pequeno script que troca o titulo do site dinamicamente.

--- meu-portifolio/src/components/AboutMe.jsx
+++ meu-portifolio/src/components/AboutMe.jsx
@@ -7,34 +7,36 @@ export function AboutMe() {
       className="fonte-cor-clara px-6 pb-28 sm:px-14 lg:px-24"
       id="Sobre-Mim"
     >
-      <h1 className="text-5xl font-bold pb-8 sm: text-center lg:text-7xl">
+      <h1 className="text-5xl font-bold pb-8 sm:text-center lg:text-7xl">
         Sobre mim
       </h1>
-      <div className="text-xl sm:grid grid-cols-2	gap-6 items-center	">
-        <p className="about-me-section border-l-4 pl-5 py-5 rounded-lg sm:mb-0">
-          Trabalho bem em equipe, valorizando a colaboração e o compartilhamento
-          de ideias para alcançar objetivos comuns. Sou proativo e persistente,
-          buscando soluções mesmo diante de desafios.
-          <br />
-          <br />
-          Atualmente, estou focado em melhorar minhas habilidades em React e
-          lógica de programação, com planos futuros de explorar o
-          desenvolvimento back-end.
-          <br />
-          <br />
-          Comecei com Python, mas logo me apaixonei pelo desenvolvimento
-          front-end. Desde 2022, tenho me dedicado a aprimorar minhas
-          habilidades nesse campo, criando projetos funcionais.
-          <br />
-          <br />
-          Além do desenvolvimento front-end, mantenho um interesse curioso em
-          pesquisar sobre criptídeos e avistamentos de aliens, o que alimenta
-          minha mente criativa para novas ideias e possibilidades.👽"
-        </p>
+      <div className="text-xl md:flex justify-between items-center gap-6 items-center h-4/5">
+        <div className="about-me-section border-l-4 pl-5 py-5 h-max rounded-lg mb-8 sm:w-full	 md:mb-0">
+          <p>
+            Trabalho bem em equipe, valorizando a colaboração e o
+            compartilhamento de ideias para alcançar objetivos comuns. Sou
+            proativo e persistente, buscando soluções mesmo diante de desafios.
+            <br />
+            <br />
+            Atualmente, estou focado em melhorar minhas habilidades em React e
+            lógica de programação, com planos futuros de explorar o
+            desenvolvimento back-end.
+            <br />
+            <br />
+            Comecei com Python, mas logo me apaixonei pelo desenvolvimento
+            front-end. Desde 2022, tenho me dedicado a aprimorar minhas
+            habilidades nesse campo, criando projetos funcionais.
+            <br />
+            <br />
+            Além do desenvolvimento front-end, mantenho um interesse curioso em
+            pesquisar sobre criptídeos e avistamentos de aliens, o que alimenta
+            minha mente criativa para novas ideias e possibilidades.👽
+          </p>
+        </div>
         <img
           src={ImagemSobreMim}
           alt="Uma pixel arte de um alien verde sentado em frente de um notebook codando."
-          className="rounded-lg"
+          className="rounded-lg md:w-2/4 h-fit"
         />
       </div>
     </section>
