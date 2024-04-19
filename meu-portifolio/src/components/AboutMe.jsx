8d5e6d3091726cbba5c3c7f105f31c4943a9d683
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-04-19 10:52:10 -0300

    Adicionando foto no sobre mim

--- meu-portifolio/src/components/AboutMe.jsx
+++ meu-portifolio/src/components/AboutMe.jsx
@@ -1,5 +1,6 @@
 /*Componente About Me*/
 /*Este componente renderiza a seção "Sobre Mim" na página.*/
+import ImagemSobreMim from "../assets/images/sobre-mim-imagem.jpeg";
 export function AboutMe() {
   return (
     <section
@@ -10,29 +11,30 @@ export function AboutMe() {
         Sobre mim
       </h1>
       <div className="text-xl sm:grid grid-cols-2	gap-6">
-        <p className="mb-8 about-me-section border-l-4 pl-5 py-5 rounded-lg sm:mb-0">
+        <p className="about-me-section border-l-4 pl-5 py-5 rounded-lg sm:mb-0">
           Comecei com Python, mas logo me encantei pelo desenvolvimento
           front-end. Desde 2022, tenho me dedicado a aprimorar minhas
           habilidades nesse campo, criando projetos funcionais e úteis para as
           pessoas.
-        </p>
-        <p className="mb-8 about-me-section border-l-4 pl-5 py-5 rounded-lg sm:mb-0">
+          <br />
+          <br />
           Atualmente, estou dedicando meu tempo ao aprimoramento das minhas
           habilidades em React e lógica de programação. Tenho planos de explorar
           o desenvolvimento back-end para criar aplicações ainda mais
           interessantes no futuro.
-        </p>
-        <p className="mb-8 about-me-section border-l-4 pl-5 py-5 rounded-lg">
+          <br />
+          <br />
           Trabalho bem em equipe, valorizando a colaboração e o compartilhamento
           de ideias para alcançar objetivos comuns. Sou proativo e persistente,
           buscando soluções mesmo diante de desafios complexos.
-        </p>
-        <p className="about-me-section border-l-4 pl-5 py-5 rounded-lg sm:mb-8">
+          <br />
+          <br />
           Além do desenvolvimento front-end, tenho um interesse curioso em
           pesquisar sobre criptídeos e avistamentos de aliens, o que me ajuda a
           manter uma mente criativa e aberta para novas ideias e
           possibilidades.👽
         </p>
+        <img src={ImagemSobreMim} alt="" className="rounded-lg" />
       </div>
     </section>
   );
