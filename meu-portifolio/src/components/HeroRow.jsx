f2118f851b71c8791581019a6c1038026a907241
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-05-27 20:44:11 -0300

    Instalei o spring no meu projeto
    Instalei o spring para criar as animações.

--- meu-portifolio/src/components/HeroRow.jsx
+++ meu-portifolio/src/components/HeroRow.jsx
@@ -2,35 +2,47 @@
 /*Este componente representa a linha de destaque na parte superior da página (hero section). Ele exibe uma breve introdução e uma imagem do autor, juntamente com links para outras seções da página.*/
 import { SocialMedias } from "./SocialMedias";
 import discoVoador from "../assets/images/disco-voador.png";
+import { useSpring, animated } from "@react-spring/web";
 
 export function HeroRow({ screenWidth }) {
+  const springs = useSpring({
+    from: { x: -100 },
+    to: { x: 0 },
+  });
   return (
     <section className="px-6 pb-28 pt-48 sm:px-14 md:flex justify-between items-center gap-6 lg:px-24">
-      <article className="fonte-cor-clara pb-8 md:w-3/5 lg:w-1/2">
-        {
-          /*Renderização condicional: exibe o componente SocialMedias apenas se a largura da tela for inferior a 900 pixels*/
-          screenWidth < 900 ? <SocialMedias /> : null
-        }
-        <h1 className="text-5xl	font-bold py-8 lg:text-7xl">Vinícius aqui!</h1>
-        <p className="text-xl pb-8">
-          Sou um desenvolvedor front-end autodidata. Estou sempre em busca de
-          novos desafios para aprimorar minhas habilidades. Trabalho com{" "}
-          <span className="font-bold">
-            {" "}
-            HTML, CSS, Bootstrap, Tailwind, JavaScript e React
-          </span>{" "}
-          para criar projetos web dinâmicos e funcionais. Estou animado para
-          continuar aprendendo e evoluindo no mundo do desenvolvimento web 🚀💻
-        </p>
-        <span className="fonte-cor-escura flex flex-wrap gap-4">
-          <a href="#Projetos" className="btn text-xl px-6 py-2 font-bold">
-            Projetos
-          </a>
-          <a href="#Sobre-Mim" className="btn text-xl px-4 py-2 font-bold">
-            Sobre mim
-          </a>
-        </span>
-      </article>
+      <animated.div
+        style={{
+          ...springs,
+        }}
+      >
+        <article className="fonte-cor-clara pb-8 md:w-3/5 lg:w-1/2">
+          {
+            /*Renderização condicional: exibe o componente SocialMedias apenas se a largura da tela for inferior a 900 pixels*/
+            screenWidth < 900 ? <SocialMedias /> : null
+          }
+          <h1 className="text-5xl	font-bold py-8 lg:text-7xl">Vinícius aqui!</h1>
+          <p className="text-xl pb-8">
+            Sou um desenvolvedor front-end autodidata. Estou sempre em busca de
+            novos desafios para aprimorar minhas habilidades. Trabalho com{" "}
+            <span className="font-bold">
+              {" "}
+              HTML, CSS, Bootstrap, Tailwind, JavaScript e React
+            </span>{" "}
+            para criar projetos web dinâmicos e funcionais. Estou animado para
+            continuar aprendendo e evoluindo no mundo do desenvolvimento web
+            🚀💻
+          </p>
+          <span className="fonte-cor-escura flex flex-wrap gap-4">
+            <a href="#Projetos" className="btn text-xl px-6 py-2 font-bold">
+              Projetos
+            </a>
+            <a href="#Sobre-Mim" className="btn text-xl px-4 py-2 font-bold">
+              Sobre mim
+            </a>
+          </span>
+        </article>
+      </animated.div>
       <figure>
         <img
           src={discoVoador}
